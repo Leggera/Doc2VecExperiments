c389012603d1a280e9848382cbcf7f424b9de842
Leggera <lana.krlv1995@gmail.com>
2017-05-08 07:22:24 +0300

    Update run_doc2vecs_IMDB_mc1.sh

--- run_doc2vecs_IMDB_mc1.sh
+++ run_doc2vecs_IMDB_mc1.sh
@@ -30,10 +30,10 @@ cat ./data/full-train-pos.txt ./data/full-train-neg.txt ./data/test-pos.txt ./da
 awk 'BEGIN{a=0;}{print "_*" a " " $0; a++;}' < alldata.txt > alldata-id.txt
 
 
-sizes=('-size 75' '-size 300')
-alphas=('-alpha 0.025' '-alpha 0.1')
-windows=('-window 5' '-window 20')
-negatives=('-negative 12' '-negative 50')
+sizes=('-size 50' '-size 75' '-size 100' '-size 200' '-size 300')
+alphas=('-alpha 0.01' '-alpha 0.015' '-alpha 0.02' '-alpha 0.025' '-alpha 0.075' '-alpha 0.1' '-alpha 0.2')
+windows=('-window 5' '-window 15' '-window 20' '-window 25')
+negatives=('-negative 6' '-negative 12' '-negative 30' '-negative 50')
 models=('-cbow 1 -sample 1e-5' '-cbow 1 -sample 1e-4' '-cbow 1 -sample 1e-3' '-cbow 0 -sample 1e-3' '-cbow 0 -sample 1e-2' '-cbow 0 -sample 1e-1')
 default_parameters=('-size 150 -alpha 0.05 -window 10 -negative 25 -iter 25 -threads 1 -min_count 1 -train alldata-id.txt')
 default_models=('-cbow 0 -sample 1e-2' '-cbow 1 -sample 1e-4')
